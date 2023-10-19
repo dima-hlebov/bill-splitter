ce7833d374ff3d5c42c700b59c30952a31bb3b8b
Dmytro <glebov.dymitriy@gmail.com>
2023-10-19 21:28:00 +0300

    fix: update cors settings

--- server/src/app.js
+++ server/src/app.js
@@ -12,6 +12,7 @@ const app = express();
 
 app.use((req, res, next) => {
     if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Origin", "https://splitmybill.vercel.app");
         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
         return res.status(200).json({});
     }
