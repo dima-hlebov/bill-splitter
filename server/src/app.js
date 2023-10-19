431d8d6c89bb9a925c9453df52791154182b52db
Dmytro <glebov.dymitriy@gmail.com>
2023-10-19 20:49:05 +0300

    fix: update cors settings

--- server/src/app.js
+++ server/src/app.js
@@ -10,12 +10,6 @@ mongoose.promise = global.Promise;
 
 const app = express();
 
-app.use(cors({
-    credentials: true,
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    origin: 'https://splitmybill.vercel.app'
-}));
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://splitmybill.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
