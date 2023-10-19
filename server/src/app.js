68244d1ffb0be49d01cb4a47f368fb6937e7a19b
Dmytro <glebov.dymitriy@gmail.com>
2023-10-19 18:25:07 +0300

    fix: update cors settings

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: 'https://splitmybill.vercel.app/'
+    origin: 'https://splitmybill.vercel.app'
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
