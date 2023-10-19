99d11f66b5f6037d754348376c188a24aac2c3cd
Dmytro <glebov.dymitriy@gmail.com>
2023-10-20 01:52:40 +0300

    fix: debug

--- server/src/app.js
+++ server/src/app.js
@@ -10,19 +10,7 @@ mongoose.promise = global.Promise;
 
 const app = express();
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'false');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-
-    if (req.method === 'OPTIONS') {
-        return res.status(200).json(({ body: 'OK' }));
-    }
-
-    return next();
-});
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: 'bill-splitter', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
@@ -30,10 +18,9 @@ app.use(session({ secret: 'bill-splitter', cookie: { maxAge: 60000 }, resave: fa
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('debug', true);
 
-
 require('./models/Users');
 require('./models/Rooms');
 require('./config/passport');
 app.use(require('./routes'));
 
-app.listen(8000, () => console.log('Server running on port - 8000'));
+app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
