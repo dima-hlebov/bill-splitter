84b3ee94db39d956e0a1dc9b4350e4b4f65f33a3
Dmytro <glebov.dymitriy@gmail.com>
2023-10-20 01:55:56 +0300

    fix: debug

--- server/src/app.js
+++ server/src/app.js
@@ -10,9 +10,7 @@ mongoose.promise = global.Promise;
 
 const app = express();
 
-app.use(cors({
-    origin: "https://splitmybill.vercel.app/"
-}));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: 'bill-splitter', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
