6acb1d602dcb193a351e37539fb2f37969f9c873
Dmytro <glebov.dymitriy@gmail.com>
2023-10-20 16:18:10 +0300

    fix: add note

--- client/src/components/pages/login-page.js
+++ client/src/components/pages/login-page.js
@@ -15,8 +15,10 @@ function LoginPage() {
             <Heading tag="h2" className="heading heading--2" text="Sign in" />
             <div className="d-flex flex-column align-items-center">
               <Form />
-              <span>Demo: demo@demo.com:demo1234</span>
-              <span >Don't have account yet? Fast<Link to="/sign-up"> sign up</Link></span>
+              <span>Don't have account yet? Fast<Link to="/sign-up"> sign up</Link></span>
+              <span className='mt-2'>Demo: demo@demo.com:demo1234</span>
+              <span>Server was sleeping.</span>
+              <span>It could take up to 1 min on a first request to load. Please be patient.</span>
             </div>
           </Col>
         </Row>
