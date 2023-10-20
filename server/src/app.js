584dea28d435ba517956b048cde4ff44874ad973
Dmytro <glebov.dymitriy@gmail.com>
2023-10-20 12:58:12 +0300

    fix: debug

--- server/src/app.js
+++ server/src/app.js
@@ -4,23 +4,24 @@ const session = require('express-session');
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const cors = require('cors');
+// const cors = require('cors');
+const cors = require('./cors');
 
 mongoose.promise = global.Promise;
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    credentials: true,
-    optionsSuccessStatus: 200
-}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: 'bill-splitter', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('debug', true);
+
+app.use((req, res, next) => {
+    cors(req, res)
+    return next()
+})
 
 require('./models/Users');
 require('./models/Rooms');
